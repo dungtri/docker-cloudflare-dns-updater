4926478f27735d33db82cd4b9ec67a56de9bbe1b
dungtri <geekwhynot@gmail.com>
2019-03-05 11:20:42 +0100

    Update README.md and add CONTRIBUTING.md, LICENSE.md.

--- docker_run.sh
+++ docker_run.sh
@@ -3,4 +3,6 @@ docker run --name dns-updater \
 -e DNS_UPDATER_EMAIL='<cloudflare email used for registration>' \
 -e DNS_UPDATER_KEY='<cloudflare api key>' \
 -e DNS_UPDATER_ZONE='<cloudflare dns zoneId to update>' \
+# (Optional) Delay between checks current ip & update when necessary (default: 30000).
+# -e SCHEDULER_CHECK_DELAY=<custom delay>
 dungtri/docker-cloudflare-dns-updater
