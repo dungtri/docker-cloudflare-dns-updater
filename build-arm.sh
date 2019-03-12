72e24c558dd3590bda600d519a82e0a5cf1ee1b6
Dung Tri LE <geekwhynot@gmail.com>
2019-03-12 22:00:46 +0000

    set arm32 docker tag

--- build-arm.sh
+++ build-arm.sh
@@ -1,1 +1,1 @@
-docker build --rm -f Dockerfile.debian-arm32-selfcontained -t dungtri/docker-cloudflare-dns-updater .
+docker build --rm -f Dockerfile.debian-arm32-selfcontained -t dungtri/docker-cloudflare-dns-updater:arm32 .
