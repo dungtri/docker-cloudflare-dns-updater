f7b3d7574fda3a4f103713ac44562444c117b4de
Dung Tri LE <geekwhynot@gmail.com>
2019-03-12 21:26:06 +0000

    Remove $

--- docker_service_create.sh
+++ docker_service_create.sh
@@ -1,4 +1,4 @@
-$ docker service create \
+docker service create \
      --name dns-updater \
      --replicas 1 \
      --secret source=dns_updater_key,target=dns_updater_key \
@@ -6,4 +6,4 @@ $ docker service create \
      -e DNS_UPDATER_EMAIL="mycloudflareaccount@me.com" \
      -e DNS_UPDATER_KEY_FILE="/run/secrets/dns_updater_key" \
      -e DNS_UPDATER_ZONE_FILE="/run/secrets/dns_updater_zone" \
-     dungtri/docker-cloudflare-dns-updater:latest
+     dungtri/docker-cloudflare-dns-updater:arm32
